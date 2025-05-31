cc808c16640af5e8dff4e592b4e8c2c24d9c69aa
sowmya <sowmya.t@everest.engineering>
2025-05-31 21:12:38 +0530

    Add addition functionality

--- trapezoidArea/main.ts
+++ trapezoidArea/main.ts
@@ -1,1 +1,20 @@
 console.log("trapezoid area....")
+
+
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+function add(a: number, b: number): number{
+    return a + b
+}
+const input1 = Number(prompt('enter value for a:'));
+const input2 = Number(prompt('enter value for b:'));
+
+if(isNaN(input1) || isNaN(input2)){
+    console.log("enter a valid input");
+}
+else{
+    const result = add(input1,input2);
+    console.log(`output: ${result}`)
+}
