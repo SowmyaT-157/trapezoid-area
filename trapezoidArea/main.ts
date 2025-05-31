c7b70327d713db2526ef5b0fad8a7c17e435ec73
sowmya <sowmya.t@everest.engineering>
2025-05-31 21:49:20 +0530

    Add functionality to multiply two values

--- trapezoidArea/main.ts
+++ trapezoidArea/main.ts
@@ -7,12 +7,30 @@ const prompt = promptSync();
 function divideByTwo(value: number): number{
     return value / 2
 }
-const input = Number(prompt("enter a number:"))
+// const input = Number(prompt("enter a number:"))
 
-if (isNaN(input)){
-    console.log("enter a valid input")
+// if (isNaN(input)){
+//     console.log("enter a valid input")
+// }
+// else{
+//     const result = divideByTwo(input);
+//     console.log(`Output:${result}`)
+// }
+
+
+function multiply(base: number, height: number): number {
+  return base * height;
 }
-else{
-    const result = divideByTwo(input);
-    console.log(`Output:${result}`)
+
+const base = Number(prompt('Enter base value b: '));
+const h = Number(prompt('Enter height value h: '));
+
+if (isNaN(base) || isNaN(h)) {
+  console.log('Please enter valid numeric values.');
+} else {
+  const result = multiply(base, h);
+  console.log(`Output: ${result}`); 
 }
+
+
+
