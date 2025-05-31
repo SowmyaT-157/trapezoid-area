0f5b3611fbad59f55860d7f54dd1c94618c0668f
sowmya <sowmya.t@everest.engineering>
2025-05-31 18:53:34 +0530

    Add a division logic for trapezoid area calculation

--- trapezoidArea/main.ts
+++ trapezoidArea/main.ts
@@ -1,1 +1,18 @@
 console.log("trapezoid area....")
+
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+function divideByTwo(value: number): number{
+    return value / 2
+}
+const input = Number(prompt("enter a number:"))
+
+if (isNaN(input)){
+    console.log("enter a valid input")
+}
+else{
+    const result = divideByTwo(input);
+    console.log(`Output:${result}`)
+}
